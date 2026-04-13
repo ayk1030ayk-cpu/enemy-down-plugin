b41bbf4c2c29874b8d7a0d624a3018ad68a8cdcd
ayk1030ayk-CPU <ayk.1030.ayk@gmail.com>
2026-04-13 16:20:32 +0900

    初回コミット（EnemyDown）

---
+++ src/main/java/plugin/enemydown/main.java
@@ -0,0 +1,15 @@
+package plugin.enemydown;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.enemydown.command.EnemyDownCommand;
+
+public final class main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this);
+    Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);// Plugin startup logic
+    getCommand("enemyDown").setExecutor(enemyDownCommand);
+  }
+}
